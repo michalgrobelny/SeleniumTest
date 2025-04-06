6bf0a0b0b3628c9afa0da903a8f9395bbefe55c5
Michał Grobelny <michal.grobelny@wp.eu>
2025-04-06 08:50:18 +0200

    Update files

--- src/main/java/SimpleTest.java
+++ src/main/java/SimpleTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.time.Duration;
 import java.io.File;
