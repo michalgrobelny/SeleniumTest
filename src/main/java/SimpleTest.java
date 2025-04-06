9aab2cebf6ad7dc2c1ee5708ae43dabb9ac4cf8e
Michał Grobelny <michal.grobelny@wp.eu>
2025-04-06 08:45:53 +0200

    Update files

---
+++ src/main/java/SimpleTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.io.File;
+
+public class SimpleTest {
+    public static void main(String[] args) throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new", "--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+
+        Thread.sleep(2000);
+
+        TakesScreenshot screenshot = ((TakesScreenshot) driver);
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        file.renameTo(new File("screenshot.png"));
+
+        driver.quit();
+    }
+}
