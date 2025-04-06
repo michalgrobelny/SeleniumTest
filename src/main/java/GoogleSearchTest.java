54af8e646712fcb23ea03e2939185d3436f41474
Michał Grobelny <michal.grobelny@wp.eu>
2025-04-06 07:49:19 +0200

    Add file import

--- src/main/java/GoogleSearchTest.java
+++ src/main/java/GoogleSearchTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.File;
 
 public class GoogleSearchTest {
     public static void main(String[] args) throws Exception {
